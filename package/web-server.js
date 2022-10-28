ab3f74e1214bf6fa493326ccf86fe8ffb9000c42
SittingBanana01 <louis.donaldson42@gmail.com>
2022-10-28 21:50:14 +0100

    started

--- package/web-server.js
+++ package/web-server.js
@@ -3,12 +3,26 @@ const fs = require("fs").promises
 const port = 8000
 
 const server = http.createServer(async (req, res) => {
-    switch (req.url) {
-        case "/":
-            fs.readFile(__dirname + `/website/index.html`)
-                .then(contents => {
-                    res.end(contents)
-                })
-            break;
+    console.log(req.url)
+    if (req.url == "/") {
+        fs.readFile(__dirname + `/website/index.html`)
+            .then(contents => {
+                res.end(contents)
+            }).catch(err => {
+                throw err;
+            })
     }
+    else if (req.url == "/index.js") {
+        fs.readFile(__dirname + `/website/index.js`)
+            .then(contents => {
+                res.end(contents)
+            }).catch(err => {
+                throw err;
+            })
+    }
+    else if (req.url.includes("/images/")) {
+        const image = await fs.readFile(__dirname + `${req.url}`)
+        res.write(image)
+    }
+
 }).listen(port)
---
+++ package/website/index.js
@@ -0,0 +1,37 @@
+const pName = "Louis";
+
+function Page() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+}
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-sm  navbar-light">
+            <div className="container-fluid">
+                <ul className="navbar-nav">
+                    <a className="navbar-brand" href="#">
+                        <img src="./images/logo.svg" alt="" />
+                    </a>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Portal</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact us</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('main_body'));
+root.render(<Page />);
