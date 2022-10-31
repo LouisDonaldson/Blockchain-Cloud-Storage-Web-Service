70c78ea2fa43f5677c0d08d48ad92358b4bf12df
Louis Donaldson <louis.donaldson42@gmail.com>
2022-10-31 13:00:50 +0000

    push

--- package/web-server.js
+++ package/web-server.js
@@ -1,28 +1,34 @@
 const http = require("http");
-const fs = require("fs").promises
-const port = 8000
+const fs = require("fs").promises;
+const port = 8000;
 
-const server = http.createServer(async (req, res) => {
-    console.log(req.url)
+const server = http
+  .createServer(async (req, res) => {
+    console.log(req.url);
     if (req.url == "/") {
-        fs.readFile(__dirname + `/website/index.html`)
-            .then(contents => {
-                res.end(contents)
-            }).catch(err => {
-                throw err;
-            })
+      fs.readFile(__dirname + `/website/index.html`)
+        .then((contents) => {
+          res.end(contents);
+        })
+        .catch((err) => {
+          throw err;
+        });
+    } else if (req.url == "/index.js") {
+      fs.readFile(__dirname + `/website/index.js`)
+        .then((contents) => {
+          res.end(contents);
+        })
+        .catch((err) => {
+          throw err;
+        });
     }
-    else if (req.url == "/index.js") {
-        fs.readFile(__dirname + `/website/index.js`)
-            .then(contents => {
-                res.end(contents)
-            }).catch(err => {
-                throw err;
-            })
+    // else if (req.url.includes("/images/")) {
+    //     const image = await fs.readFile(__dirname + `${req.url}`)
+    //     res.write(image)
+    // }
+    else {
+      res.writeHead(404);
+      res.end("<h1>404 Not found</h1>");
     }
-    else if (req.url.includes("/images/")) {
-        const image = await fs.readFile(__dirname + `${req.url}`)
-        res.write(image)
-    }
-
-}).listen(port)
+  })
+  .listen(port);
