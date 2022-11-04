ebe3c62d6e13113c2d1156f45a16f9ec8ca67c2e
Louis Donaldson <louis.donaldson42@gmail.com>
2022-11-04 14:06:03 +0000

    added log in section

--- package/website/index.js
+++ package/website/index.js
@@ -11,16 +11,28 @@ function Page() {
 function Navbar() {
   return (
     <div className="page_nav">
-      <ul className="nav_ul">
-        <li>
-          <img src="./images/logo_icon.svg" className="navbar_logo" />
-        </li>
-        <li className="nav_item active">Home</li>
-        <li className="nav_item">Portal</li>
-        <li className="nav_item">Contact us</li>
-      </ul>
+      <div className="nav_ul">
+        <div className="nav_left">
+          <div>
+            <img src="./images/logo_icon.svg" className="navbar_logo" />
+          </div>
+          <div className="nav_item active" id="home_btn">
+            Home
+          </div>
+          <div className="nav_item" id="portal_btn">
+            Portal
+          </div>
+          <div className="nav_item" id="contact_btn">
+            Contact us
+          </div>
+        </div>
+        <div className="nav_right">
+          <div className="nav_item log_in_btn" id="log_in_btn">
+            Log in
+          </div>
+        </div>
+      </div>
     </div>
-
   );
 }
 
